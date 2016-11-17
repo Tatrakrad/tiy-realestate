1d147e46708e8574e9afcdb9b009aef28e5ab885
Tatrakrad <riddick.taylor@gmail.com>
2016-11-17 14:08:04 -0600

    basic page can serve static files

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'))
+
+
+app.get('/', function(req, res){
+ res.sendFile('/index.html');
+
+});
+//
 
 app.listen(3001, function() {
   console.log('Listening on port 3001');
