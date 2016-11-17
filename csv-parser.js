bcf52fd530211e358c786e9e05e6c2c58407071e
Tatrakrad <riddick.taylor@gmail.com>
2016-11-17 17:43:55 -0600

    for testing, csv will appear after request made in index

--- csv-parser.js
+++ csv-parser.js
@@ -5,15 +5,14 @@ var fs = require('fs');
 function parse(callback){
   var callbackForReadFile = function(err,fileContents){
 
-    var output = {
-      fileContents;
-    }
+    var output = fileContents.split('\r');
 
     callback(output);
   };
 
 
   fs.readFile('Sacramentorealestatetransactions.csv','utf-8',callbackForReadFile);
+  //this file is split with /r!
   console.log("reading file on disk");
 
 }
